241682805c194e14222a4b37acc12f706b58f12e
ali1924 <mohammadali229987@gmail.com>
2023-04-07 16:30:01 +0600

    51-7 (super advanced) Handle quantity from storage to cart

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -34,7 +34,21 @@ const Shop = () => {
 
     const handleAddToCart = (product) => {
         // console.log(product);
-        const newCart = [...cart, product];
+        // const newCart = [...cart, product];
+        // option-2 quantity fixed
+        let newCart = [];
+        // Step-1 if doesn't exist in  the cart, then set quantity=1
+        const exists = cart.find(pd => pd.id === product.id);
+        if (!exists) {
+            product.quantity = 1;
+            newCart = [...cart, product];
+        }
+        else {
+            exists.quantity += 1;
+            const remaining = cart.filter(pd => pd.id !== product.id);
+            newCart = [...remaining, exists];
+        }
+
         setCart(newCart);
         addToDb(product.id);
     }
