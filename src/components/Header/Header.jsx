9874840b945bc93ef894e2f700bb1c1b484b2495
ali1924 <mohammadali229987@gmail.com>
2023-04-07 10:56:09 +0600

    50-2 Header Component, logo position, set nav menu

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
-
+import './Header.css';
+import logo from '../../images/Logo.svg';
 const Header = () => {
     return (
-        <div>
-            <h2>This is Header components</h2>
-        </div>
+        <nav className='header'>
+            <img src={logo} alt="" />
+            <div className=''>
+                <a href="/shop">Shop</a>
+                <a href="/shop-review">Shop Review</a>
+                <a href="/manage-inventory">Manage Inventory</a>
+                <a href="/login">Login</a>
+            </div>
+        </nav>
     );
 };
 
